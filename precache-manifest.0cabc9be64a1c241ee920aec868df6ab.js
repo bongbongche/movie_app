ee1e39ce15110f62907b9b22e0381deb5609213b
bongbong <60419514+bongbongche@users.noreply.github.com>
2020-06-04 15:57:48 +0900

    Updates

---
+++ precache-manifest.0cabc9be64a1c241ee920aec868df6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfa4e802b867453c40d3b59ae096172",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "38ce278a461bcc220ae7",
+    "url": "/movie_app/static/css/main.17e33fde.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ce278a461bcc220ae7",
+    "url": "/movie_app/static/js/main.4638fa24.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
